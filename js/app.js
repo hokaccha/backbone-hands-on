e174a4a3d95193967123685ae8f9fcb1396ca54e
Kazuhito Hokamura <k.hokamura@gmail.com>
2013-10-22 18:24:21 +0900

    03: Modelのイベントや機能の実装

--- js/app.js
+++ js/app.js
@@ -3,12 +3,18 @@ window.App = {};
 $(function() {
   var schedule = new App.Schedule();
 
+  schedule.on('change', function() {
+    $('body').html(
+      schedule.dateFormat('MM月DD日 HH時mm分') + '：' + schedule.get('title')
+    );
+  });
+
+  schedule.on('invalid', function(model, message) {
+    alert('Error: ' + message);
+  });
+
   schedule.set({
     'title': '打ち合わせ',
     'datetime': moment('2013-10-26 15:00')
-  });
-
-  $('body').html(
-    schedule.get('datetime').format('MM月DD日 HH時mm分') + '：' + schedule.get('title')
-  );
+  }, { validate: true });
 });
--- js/model.js
+++ js/model.js
@@ -1,1 +1,20 @@
-App.Schedule = Backbone.Model.extend();
+App.Schedule = Backbone.Model.extend({
+  defaults: {
+    title: null,
+    datetime: null
+  },
+  validate: function(attrs) {
+    if (!attrs.title) {
+      return 'タイトルは必須です';
+    }
+    if (!attrs.datetime) {
+      return '日時は必須です';
+    }
+    if (!moment.isMoment(attrs.datetime) || !attrs.datetime.isValid()) {
+      return '日時の形式が不正です';
+    }
+  },
+  dateFormat: function(f) {
+    return this.get('datetime').format(f);
+  }
+});
