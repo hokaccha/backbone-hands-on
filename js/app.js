f19d162969fb38738a4b2676535d691c321ad12f
Kazuhito Hokamura <k.hokamura@gmail.com>
2013-10-22 18:10:54 +0900

    02: Modelを作成しsetとgetを使う

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 window.App = {};
 
 $(function() {
-  $('body').html('Hello Backbone.js ' + Backbone.VERSION);
+  var schedule = new App.Schedule();
+
+  schedule.set({
+    'title': '打ち合わせ',
+    'datetime': moment('2013-10-26 15:00')
+  });
+
+  $('body').html(
+    schedule.get('datetime').format('MM月DD日 HH時mm分') + '：' + schedule.get('title')
+  );
 });
